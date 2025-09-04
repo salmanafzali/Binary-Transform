efc0743d5608ce622695a2bb7eea40bd6dcac5e9
salman <ernestsalman05@gmail.com>
2025-09-04 17:54:45 +0430

    change entry for get

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ def binary():
 window = tk.Tk()
 window.geometry('550x420')
 window.title("تبدیل مبنا")
+window.maxsize(550, 420)
 
 # upload image
 # main_img = PhotoImage(file="ed-1.png")
@@ -31,14 +32,16 @@ input_num = Label(window, text="عدد وارد شده = ", font=("", "12")).pla
 conv_2 = Label(window, text="تبدیل به 2 = ", font=("", "12")).place(x=47, y=80)
 conv_8 = Label(window, text="تبدیل به 8 = ", font=("", "12")).place(x=47, y=130)
 conv_16 = Label(window, text="تبدیل به 16 = ", font=("", "12")).place(x=39, y=180)
-dec_int_number = Entry(window, font=("", "14"), background='darkblue').place(x=30, y=230, width=170)
+dec_int_number = Entry(window, font=("", "14"), background='darkblue')
+dec_int_number.place(x=30, y=230, width=170)
 
 # converting base other number
 conv_10_base = Label(window, text="تبدیل به 10 = ", font=("", "12")).place(x=354, y=30)
 conv_2_base = Label(window, text="تبدیل به 2 = ", font=("", "12")).place(x=362, y=80)
 conv_8_base = Label(window, text="تبدیل به 8 = ", font=("", "12")).place(x=362, y=130)
 conv_16_base = Label(window, text="تبدیل به 16 = ", font=("", "12")).place(x=354, y=180)
-binary_number = Entry(window, font=("", "14")).place(x=345, y=230, width=170)
+binary_number = Entry(window, font=("", "14"))
+binary_number.place(x=345, y=230, width=170)
 
 # Button Convert
 Button(window, text="تبدیل عدد صحیح", command=integer).place(x=50, y=280, width=120, height=50)
